54fcaac65effca19e32ee3de4b02d90208a76b67
medis <medisadido@gamil.com>
2024-01-05 01:32:55 +0100

    mm

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter,MonteCarlo ,Lusitana} from 'next/font/google';
+
+
+export const inter = Inter({ subsets: ['latin'] });
+export const mountercarlo = MonteCarlo({ subsets: ['latin'],weight:'400' });
+export const lusitana = Lusitana({ subsets: ['latin'],weight:['400','700'] });
